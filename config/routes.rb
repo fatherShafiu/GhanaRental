15032d3cf29ef6fc3755aaf6325182ca69e7efba
Tonon Abdul-Shafiu Mahmud <tononabdulshafiumahmud@gmail.com>
2025-09-08 23:26:22 +0000

    enabled initialization

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'pages#home'
+  # Devise routes with custom controllers
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  }
+
+  # Root path
+  root "pages#home"
+
+  # User dashboard based on role
+  get "dashboard", to: "dashboards#show"
+
+  # Additional routes will be added here as we progress
 end
