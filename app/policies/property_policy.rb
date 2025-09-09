3013bb27740da6dc28cc4e53579b2ead691664fb
Tonon Abdul-Shafiu Mahmud <tononabdulshafiumahmud@gmail.com>
2025-09-09 11:39:26 +0000

    enabled initialization

---
+++ app/policies/property_policy.rb
@@ -0,0 +1,49 @@
+class PropertyPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if admin?
+        scope.all
+      elsif landlord?
+        scope.where(landlord: user)
+      else
+        scope.published.available
+      end
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    admin? || record.published? || (landlord? && record.landlord == user)
+  end
+
+  def create?
+    landlord? || admin?
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    admin? || (landlord? && record.landlord == user)
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    admin? || (landlord? && record.landlord == user)
+  end
+
+  def publish?
+    update?
+  end
+
+  def archive?
+    update?
+  end
+end
