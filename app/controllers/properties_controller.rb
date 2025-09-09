3b576e3836dc55d2eae20c5be366b6f165bb421e
Tonon Abdul-Shafiu Mahmud <tononabdulshafiumahmud@gmail.com>
2025-09-09 11:40:03 +0000

    enabled initialization

---
+++ app/controllers/properties_controller.rb
@@ -0,0 +1,84 @@
+class PropertiesController < ApplicationController
+  before_action :authenticate_user!, except: [ :index, :show ]
+  before_action :set_property, only: [ :show, :edit, :update, :destroy, :publish, :archive ]
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
+
+  def index
+    @properties = policy_scope(Property).includes(:landlord, images_attachments: :blob)
+
+    # Filtering
+    @properties = @properties.by_city(params[:city]) if params[:city].present?
+    @properties = @properties.by_price_range(params[:min_price].to_f, params[:max_price].to_f) if params[:min_price].present? && params[:max_price].present?
+    @properties = @properties.where(property_type: params[:property_type]) if params[:property_type].present?
+    @properties = @properties.where(bedrooms: params[:bedrooms]) if params[:bedrooms].present?
+
+    # Pagination
+    @pagy, @properties = pagy(@properties.order(created_at: :desc), items: 12)
+  end
+
+  def show
+    authorize @property
+  end
+
+  def new
+    @property = current_user.properties.new
+    authorize @property
+  end
+
+  def create
+    @property = current_user.properties.new(property_params)
+    authorize @property
+
+    if @property.save
+      redirect_to @property, notice: "Property was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    authorize @property
+  end
+
+  def update
+    authorize @property
+    if @property.update(property_params)
+      redirect_to @property, notice: "Property was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    authorize @property
+    @property.destroy
+    redirect_to properties_url, notice: "Property was successfully destroyed."
+  end
+
+  def publish
+    authorize @property
+    @property.published!
+    redirect_to @property, notice: "Property is now published."
+  end
+
+  def archive
+    authorize @property
+    @property.archived!
+    redirect_to @property, notice: "Property has been archived."
+  end
+
+  private
+
+  def set_property
+    @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(
+      :title, :description, :property_type, :price, :bedrooms,
+      :bathrooms, :square_feet, :address, :city, :state, :zip_code,
+      :available_from, :featured, images: [], floor_plan: []
+    )
+  end
+end
