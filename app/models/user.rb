11739ed927cc2fd7b6909390418972e91a6de943
Tonon Abdul-Shafiu Mahmud <tononabdulshafiumahmud@gmail.com>
2025-09-11 11:38:56 +0000

    worked on conversations

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,12 +16,26 @@ class User < ApplicationRecord
     has_many :favorites, dependent: :destroy
   has_many :favorited_properties, through: :favorites, source: :property
 
+    has_many :conversations_as_sender, class_name: "Conversation", foreign_key: "sender_id", dependent: :destroy
+  has_many :conversations_as_recipient, class_name: "Conversation", foreign_key: "recipient_id", dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :notifications, dependent: :destroy
+
   def favorited?(property)
     favorites.exists?(property: property)
   end
 
   # Callbacks
   after_create :create_profile
+
+
+  def conversations
+    Conversation.where(sender: self).or(Conversation.where(recipient: self))
+  end
+
+  def unread_notifications_count
+    notifications.unread.count
+  end
 
   private
 
