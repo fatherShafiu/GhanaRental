c4b3881f9aa2100d6a8ccb9a4eb3a2ddfa08213c
Tonon Abdul-Shafiu Mahmud <tononabdulshafiumahmud@gmail.com>
2025-09-11 16:28:06 +0000

    converstaions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,18 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def application_status_class(status)
+    case status.to_sym
+    when :pending
+      "bg-yellow-100 text-yellow-800"
+    when :approved
+      "bg-green-100 text-green-800"
+    when :rejected
+      "bg-red-100 text-red-800"
+    when :withdrawn
+      "bg-gray-100 text-gray-800"
+    else
+      "bg-gray-100 text-gray-800"
+    end
+  end
 end
