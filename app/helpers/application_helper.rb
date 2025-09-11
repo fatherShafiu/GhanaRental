2fcd9f8309572a898990de995598b1ac3a6a8b73
Tonon Abdul-Shafiu Mahmud <tononabdulshafiumahmud@gmail.com>
2025-09-11 18:56:55 +0000

    payments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,19 @@ module ApplicationHelper
       "bg-gray-100 text-gray-800"
     end
   end
+
+  def payment_status_class(status)
+   case status.to_sym
+   when :pending
+        "bg-yellow-100 text-yellow-800"
+   when :completed
+        "bg-green-100 text-green-800"
+   when :failed
+        "bg-red-100 text-red-800"
+   when :refunded
+        "bg-blue-100 text-blue-800"
+   else
+        "bg-gray-100 text-gray-800"
+   end
+  end
 end
