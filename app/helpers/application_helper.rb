9120e6d46c70ea6ca6af39e22199d0288da5c772
Tonon Abdul-Shafiu Mahmud <tononabdulshafiumahmud@gmail.com>
2025-09-12 11:52:45 +0000

    documents

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,6 +15,20 @@ module ApplicationHelper
       "bg-gray-100 text-gray-800"
     end
   end
+   def document_type_badge(document_type)
+    case document_type.to_sym
+    when :id_proof
+      "bg-blue-100 text-blue-800"
+    when :proof_of_income
+      "bg-green-100 text-green-800"
+    when :employment_verification
+      "bg-purple-100 text-purple-800"
+    when :rental_history
+      "bg-yellow-100 text-yellow-800"
+    else
+      "bg-gray-100 text-gray-800"
+    end
+   end
 
   def payment_status_class(status)
    case status.to_sym
